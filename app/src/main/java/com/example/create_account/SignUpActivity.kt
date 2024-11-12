1f4aab0ae79244cd4bac9608f5f19f75ba9807e6
Oksana <oksa.osidach@gmail.com>
2024-11-12 02:34:26 +0100

    navigation on labelLogIn

--- app/src/main/java/com/example/create_account/SignUpActivity.kt
+++ app/src/main/java/com/example/create_account/SignUpActivity.kt
@@ -1,5 +1,8 @@
 package com.example.create_account
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class RegisterActivity : AppCompatActivity() {
@@ -7,5 +10,13 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sign_up)
+        val labelLogIn = findViewById<TextView>(R.id.labelLogIn)
+
+        labelLogIn.setOnClickListener {
+            Log.d("Onboarding", "Pressed register now label")
+
+            val goToRegisterIntent = Intent(this@RegisterActivity, LogInActivity::class.java)
+            startActivity(goToRegisterIntent)
+        }
     }
 }
