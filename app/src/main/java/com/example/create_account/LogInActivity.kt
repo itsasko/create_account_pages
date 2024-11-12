d7e0c8170a68377dafbd89c2b8c0d5a7b0b8db53
Oksana <oksa.osidach@gmail.com>
2024-11-12 02:02:20 +0100

    renaming files, navigation on labelRegisterNow

--- app/src/main/java/com/example/create_account/LogInActivity.kt
+++ app/src/main/java/com/example/create_account/LogInActivity.kt
@@ -1,20 +1,31 @@
 package com.example.create_account
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class LogInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.welcome_back)
+        setContentView(R.layout.log_in)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val labelRegisterNow = findViewById<TextView>(R.id.labelRegisterNow)
+
+        labelRegisterNow.setOnClickListener {
+            Log.d("Onboarding", "Pressed register now label")
+
+            val goToRegisterIntent = Intent(this@LogInActivity, RegisterActivity::class.java)
+            startActivity(goToRegisterIntent)
         }
     }
 }
---
+++ app/src/main/java/com/example/create_account/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package com.example.create_account
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_up)
+    }
+}
